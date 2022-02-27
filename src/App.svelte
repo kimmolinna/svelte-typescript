87e068ee59cf53b86814baf9e375521f8a123eac
Kimmo Linna <kimmo.linna@gmail.com>
2022-02-27 09:23:50 +0200

    initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,25 @@
 	import * as duckdb from '@duckdb/duckdb-wasm';
 
 	export let db: Promise<duckdb.AsyncDuckDB>;
+	export let conn: Promise<duckdb.AsyncDuckDBConnection> = connection(); 
+	export let res: Promise<Table<T>>;
+
+	async function connection(){
+		const conn = await (await db).connect();
+		return conn;
+	}
+	
+	async function query(query: string){
+		const res = await (await conn).query(query);
+		return res;	
+	}
+	
+	res = query('SELECT count(*)::INTEGER as v FROM generate_series(0, 100) t(v)')
+	
 </script>
 
 <main>
+
 	{#await db}
 		<h1>Instantiating...</h1>
 	{:then db} 
@@ -12,6 +28,21 @@
 	{:catch error}
 		<p style="color: red">{error.message}</p>
 	{/await}
+	{#await conn}
+		<p>...waiting</p>
+	{:then conn}
+		<p>The connection is established.</p>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+	{#await res}
+		<h1>...executing</h1>
+	{:then res} 
+		<h1>{res.toArray()}</h1>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+
 </main>
 
 <style>
